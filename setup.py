ed573b4900dc9ed3ee31a7903feb64fefd723617
Yauheni Kachan <19803638+bagxi@users.noreply.github.com>
2021-08-05 23:50:47 +0300

    deploy-on-release github action added

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Note: To use the "upload" functionality of this file, you must:
-#   $ pip install twine
+from typing import Iterable, Union
+from pathlib import Path
 
-import io
-import os
-from shutil import rmtree
-import sys
-
-from setuptools import Command, find_packages, setup
+from setuptools import find_packages, setup
 
 # Package meta-data.
 NAME = "catalyst-codestyle"
@@ -20,60 +15,19 @@ EMAIL = "scitator@gmail.com"
 AUTHOR = "Sergey Kolesnikov"
 REQUIRES_PYTHON = ">=3.6.0"
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = Path(__file__).parent.resolve()
 
 
-def load_requirements(filename):
+def load_requirements(filename: Union[Path, str] = "requirements.txt") -> Iterable[str]:
     """Load package requirements."""
-    with open(os.path.join(PROJECT_ROOT, filename), "r") as f:
+    with open(PROJECT_ROOT / filename) as f:
         return f.read().splitlines()
 
 
-def load_readme():
+def load_readme(filename: Union[Path, str] = "README.md") -> str:
     """Load package readme."""
-    readme_path = os.path.join(PROJECT_ROOT, "README.md")
-    with io.open(readme_path, encoding="utf-8") as f:
+    with open(PROJECT_ROOT / filename, encoding="utf-8") as f:
         return f"\n{f.read()}"
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = "Build and publish the package."
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print(f"\033[1m{s}\033[0m")
-
-    def initialize_options(self):
-        """@TODO: Docs. Contribution is welcome"""
-        pass
-
-    def finalize_options(self):
-        """@TODO: Docs. Contribution is welcome"""
-        pass
-
-    def run(self):
-        """Run upload command."""
-        try:
-            self.status("Removing previous builds…")
-            rmtree(os.path.join(PROJECT_ROOT, "dist"))
-        except OSError:
-            pass
-
-        self.status("Building Source and Wheel (universal) distribution…")
-        os.system(f"{sys.executable} setup.py sdist bdist_wheel --universal")  # noqa: E501, S605
-
-        self.status("Uploading the package to PyPI via Twine…")
-        os.system("twine upload dist/*")  # noqa: S605, S607
-
-        self.status("Pushing git tags…")
-        os.system(f"git tag v{VERSION}")  # noqa: S605
-        os.system("git push --tags")  # noqa: S605, S607
-
-        sys.exit()
 
 
 setup(
@@ -92,14 +46,14 @@ setup(
         "Documentation": "https://catalyst-team.github.io/catalyst",
         "Source Code": "https://github.com/catalyst-team/codestyle",
     },
-    packages=find_packages(exclude=("tests", )),
+    packages=find_packages(exclude=("tests",)),
     scripts=[
         "bin/catalyst-check-codestyle",
         "bin/catalyst-make-codestyle",
         "bin/catalyst-codestyle-flake8",
         "bin/catalyst-codestyle-isort",
     ],
-    install_requires=load_requirements("requirements.txt"),
+    install_requires=load_requirements(),
     include_package_data=True,
     license="Apache License 2.0",
     classifiers=[
@@ -115,10 +69,8 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
-    # $ setup.py publish support.
-    cmdclass={
-        "upload": UploadCommand,
-    },
 )
